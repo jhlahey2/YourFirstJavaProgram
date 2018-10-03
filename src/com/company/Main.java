be22c520fafc997f6af8996ba9e9ad5ed853362b
jhlahey2 <jhlahey@protonmail.com>
2018-10-03 17:25:24 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+/***********************************************************
+ * Assignment
+ * Copy Hello World, then add your name
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Today is July 19, 2015. I am alive!");
+        System.out.println("my name is Jack");
+    }
+
+}
